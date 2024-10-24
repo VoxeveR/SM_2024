56cef40093488d08bca80c8ae9a2d96bec0e3c47
VoxeveR <piotrzietek01@gmail.com>
2024-10-24 23:18:46 +0200

    LAB3 lap to pc

---
+++ LAB3/src/main.cpp
@@ -0,0 +1,570 @@
+#include <exception>
+#include <string.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <bitset>
+
+using namespace std;
+
+SDL_Window* window = NULL;
+SDL_Surface* screen = NULL;
+
+#define szerokosc 640
+#define wysokosc 400
+
+#define tytul "SM2024 - Lab 3 - Zietek Piotr"
+
+void setPixel(int x, int y, Uint8 R, Uint8 G, Uint8 B);
+SDL_Color getPixel (int x, int y);
+
+void linia(int x1, int y1, int x2, int y2, Uint8 R, Uint8 G, Uint8 B);
+
+void czyscEkran(Uint8 R, Uint8 G, Uint8 B);
+
+void Funkcja1();
+void Funkcja2();
+void Funkcja3();
+void Funkcja4();
+void Funkcja5();
+void Funkcja6();
+void Funkcja7();
+void Funkcja8();
+void Funkcja9();
+void setRGB555(int xx, int yy, Uint8 r, Uint8 g, Uint8 b);
+void setRGB555(int xx, int yy, Uint16 rgb555);
+void setRGB565(int xx, int yy, Uint8 r, Uint8 g, Uint8 b);
+void setRGB565(int xx, int yy, Uint16 rgb565);
+SDL_Color getRGB555(int xx, int yy);
+Uint16 getRGB555_(int xx, int yy);
+SDL_Color getRGB565(int xx, int yy);
+Uint16 getRGB565_(int xx, int yy);
+
+void Funkcja1() {
+
+    for(int x = 0; x < szerokosc/2; x++){
+        for(int y = 0; y < wysokosc/2; y++){
+            SDL_Color color = getPixel(x, y);
+            setRGB555(x + szerokosc/2, y, color.r, color.g, color.b);
+        }
+    }
+
+    SDL_UpdateWindowSurface(window);
+}
+
+void Funkcja2() {
+
+       for(int x = 0; x < szerokosc/2; x++){
+        for(int y = 0; y < wysokosc/2; y++){
+            SDL_Color color = getRGB555(x, y);
+            setRGB555(x, y + wysokosc/2, color.r, color.g, color.b);
+        }
+    }
+
+    SDL_UpdateWindowSurface(window);
+}
+
+void Funkcja3() {
+
+    for(int x = 0; x < szerokosc/2; x++){
+        for(int y = 0; y < wysokosc/2; y++){
+            Uint16 color = getRGB555_(x, y);
+            setRGB555(x, y + wysokosc/2, color);    // set nie dziala         
+            //std::bitset<16> x(color);
+            //std::cout << x << std::endl;
+        }
+    }
+
+    SDL_UpdateWindowSurface(window);
+}
+
+void Funkcja4() {
+
+    //...
+
+    SDL_UpdateWindowSurface(window);
+}
+
+void Funkcja5() {
+
+    //...
+
+    SDL_UpdateWindowSurface(window);
+}
+
+void Funkcja6() {
+
+    //...
+
+    SDL_UpdateWindowSurface(window);
+}
+
+void Funkcja7() {
+
+    //...
+
+    SDL_UpdateWindowSurface(window);
+}
+
+void Funkcja8() {
+
+    //...
+
+    SDL_UpdateWindowSurface(window);
+}
+
+void Funkcja9() {
+
+    //...
+
+    SDL_UpdateWindowSurface(window);
+}
+
+void setRGB555(int xx, int yy, Uint8 r, Uint8 g, Uint8 b){
+    r = r >> 3;
+    g = g >> 3;
+    b = b >> 3;
+
+    r = r << 3;
+    g = g << 3;
+    b = b << 3;
+
+    setPixel(xx, yy, r, g, b);
+}
+
+void setRGB555(int xx, int yy, Uint16 rgb555){
+    Uint8 r, g, b;
+
+    std::bitset<16> x2(rgb555);
+    //std::cout<< "PRZED: " << x2 << std::endl;
+
+    r = ((((rgb555 << 1) >> 12) << 3));
+
+    g = ((((rgb555 << 5) >> 11) << 2));
+    b = ((((rgb555 << 11) >> 11) << 2));
+
+    std::bitset<8> x(r);
+   // std::cout<< "PO: " << x << std::endl;
+
+    setPixel(xx, yy, r, g, b);
+    return;
+}
+
+void setRGB565(int xx, int yy, Uint8 r, Uint8 g, Uint8 b){
+    r = r >> 3;
+    g = g >> 2;
+    b = b >> 3;
+
+    r = r << 3;
+    g = g << 2;
+    b = b << 3;
+
+    setPixel(xx, yy, r, g, b);
+}
+
+void setRGB565(int xx, int yy, Uint16 rgb565){
+
+
+    return;
+}
+
+SDL_Color getRGB555(int xx, int yy){
+    SDL_Color color = getPixel(xx, yy);
+    
+    color.r = color.r >> 3;
+    color.g = color.g >> 2;
+    color.b = color.b >> 3;
+
+    color.r = color.r << 3;
+    color.g = color.g << 2;
+    color.b = color.b << 3;
+
+    return color;
+}
+
+Uint16 getRGB555_(int xx, int yy){
+
+    SDL_Color color = getPixel(xx, yy);
+    Uint16 retColor, r, g, b;
+
+    r = color.r;
+    g = color.g;
+    b = color.b;
+
+    std::bitset<16> x1(r);
+    std::bitset<16> x2(g);
+    std::bitset<16> x3(b);
+
+   // std::cout<<"PRZED: " << x1<< " " << x2 << " " << x3 << std::endl;
+    r = (r >> 3) << 10;
+    g = (g >> 3) << 5;
+    b = (b >> 3);
+    std::bitset<16> x4(r);
+    std::bitset<16> x5(g);
+    std::bitset<16> x6(b);
+
+  //  std::cout<<"PRZED: " << x4<< " " << x5 << " " << x6 << std::endl;
+
+
+    retColor = r | g | b;
+    std::bitset<16> x(retColor);
+   // std::cout<<"PO: " << x<<std::endl;
+    return retColor;
+}
+
+SDL_Color getRGB565(int xx, int yy){
+
+    SDL_Color color;
+    return color;
+}
+
+Uint16 getRGB565_(int xx, int yy){
+
+    Uint16 dupa;
+    return dupa;
+}
+
+
+void linia(int x1, int y1, int x2, int y2, Uint8 R, Uint8 G, Uint8 B) {
+
+    int dx = x2-x1;
+    int dy = y2-y1;
+
+    int kx = 1;
+    int ky = 1;
+
+    if (dx<0) {
+        dx = x1-x2;
+        kx = -1;
+    }
+
+    if (dy<0) {
+        dy = y1-y2;
+        ky = -1;
+    }
+
+    int d = 0;
+
+    int x = x1; //
+    int y = y1; //
+
+    setPixel(x, y, R, G, B);
+
+    if (dx >= dy) {
+        d = 2*dy - dx;
+
+        while (x != x2) {
+            if (d >=0) {
+                x+=kx;
+                y+=ky;
+                d = d + 2*(dy-dx);
+            }
+            else {
+                x+=kx;
+                d = d + 2*dy;
+            }
+            setPixel(x, y, R, G, B);
+        }
+    }
+    else {
+        d = 2*dx - dy;
+        while (y != y2) {
+            if (d >= 0) {
+                y+=ky;
+                x+=kx;
+                d = d + 2*(dx-dy);
+            }
+            else {
+                y+=ky;
+                d = d + 2*dx;
+            }
+            setPixel(x, y, R, G, B);
+        }
+
+    }
+
+    SDL_UpdateWindowSurface(window);
+}
+
+
+void setPixel(int x, int y, Uint8 R, Uint8 G, Uint8 B)
+{
+  if ((x>=0) && (x<szerokosc) && (y>=0) && (y<wysokosc))
+  {
+    /* Zamieniamy poszczególne składowe koloru na format koloru piksela */
+    Uint32 pixel = SDL_MapRGB(screen->format, R, G, B);
+
+    /* Pobieramy informację ile bajtów zajmuje jeden piksel */
+    int bpp = screen->format->BytesPerPixel;
+
+    /* Obliczamy adres piksela */
+    Uint8 *p1 = (Uint8 *)screen->pixels + (y*2) * screen->pitch + (x*2) * bpp;
+    Uint8 *p2 = (Uint8 *)screen->pixels + (y*2+1) * screen->pitch + (x*2) * bpp;
+    Uint8 *p3 = (Uint8 *)screen->pixels + (y*2) * screen->pitch + (x*2+1) * bpp;
+    Uint8 *p4 = (Uint8 *)screen->pixels + (y*2+1) * screen->pitch + (x*2+1) * bpp;
+
+    /* Ustawiamy wartość piksela, w zależnoœci od formatu powierzchni*/
+    switch(bpp)
+    {
+        case 1: //8-bit
+            *p1 = pixel;
+            *p2 = pixel;
+            *p3 = pixel;
+            *p4 = pixel;
+            break;
+
+        case 2: //16-bit
+            *(Uint16 *)p1 = pixel;
+            *(Uint16 *)p2 = pixel;
+            *(Uint16 *)p3 = pixel;
+            *(Uint16 *)p4 = pixel;
+            break;
+
+        case 3: //24-bit
+            if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+                p1[0] = (pixel >> 16) & 0xff;
+                p1[1] = (pixel >> 8) & 0xff;
+                p1[2] = pixel & 0xff;
+                p2[0] = (pixel >> 16) & 0xff;
+                p2[1] = (pixel >> 8) & 0xff;
+                p2[2] = pixel & 0xff;
+                p3[0] = (pixel >> 16) & 0xff;
+                p3[1] = (pixel >> 8) & 0xff;
+                p3[2] = pixel & 0xff;
+                p4[0] = (pixel >> 16) & 0xff;
+                p4[1] = (pixel >> 8) & 0xff;
+                p4[2] = pixel & 0xff;
+            } else {
+                p1[0] = pixel & 0xff;
+                p1[1] = (pixel >> 8) & 0xff;
+                p1[2] = (pixel >> 16) & 0xff;
+                p2[0] = pixel & 0xff;
+                p2[1] = (pixel >> 8) & 0xff;
+                p2[2] = (pixel >> 16) & 0xff;
+                p3[0] = pixel & 0xff;
+                p3[1] = (pixel >> 8) & 0xff;
+                p3[2] = (pixel >> 16) & 0xff;
+                p4[0] = pixel & 0xff;
+                p4[1] = (pixel >> 8) & 0xff;
+                p4[2] = (pixel >> 16) & 0xff;
+            }
+            break;
+
+        case 4: //32-bit
+            *(Uint32 *)p1 = pixel;
+            *(Uint32 *)p2 = pixel;
+            *(Uint32 *)p3 = pixel;
+            *(Uint32 *)p4 = pixel;
+            break;
+
+        }
+    }
+}
+
+void setPixelSurface(int x, int y, Uint8 R, Uint8 G, Uint8 B)
+{
+  if ((x>=0) && (x<szerokosc*2) && (y>=0) && (y<wysokosc*2))
+  {
+    /* Zamieniamy poszczególne składowe koloru na format koloru piksela */
+    Uint32 pixel = SDL_MapRGB(screen->format, R, G, B);
+
+    /* Pobieramy informację ile bajtów zajmuje jeden piksel */
+    int bpp = screen->format->BytesPerPixel;
+
+    /* Obliczamy adres piksela */
+    Uint8 *p = (Uint8 *)screen->pixels + y * screen->pitch + x * bpp;
+
+    /* Ustawiamy wartość piksela, w zależności od formatu powierzchni*/
+    switch(bpp)
+    {
+        case 1: //8-bit
+            *p = pixel;
+            break;
+
+        case 2: //16-bit
+            *(Uint16 *)p = pixel;
+            break;
+
+        case 3: //24-bit
+            if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+                p[0] = (pixel >> 16) & 0xff;
+                p[1] = (pixel >> 8) & 0xff;
+                p[2] = pixel & 0xff;
+            } else {
+                p[0] = pixel & 0xff;
+                p[1] = (pixel >> 8) & 0xff;
+                p[2] = (pixel >> 16) & 0xff;
+            }
+            break;
+
+        case 4: //32-bit
+            *(Uint32 *)p = pixel;
+            break;
+        }
+    }
+}
+
+SDL_Color getPixel(int x, int y) {
+    SDL_Color color ;
+    Uint32 col = 0 ;
+    if ((x>=0) && (x<szerokosc) && (y>=0) && (y<wysokosc)) {
+        //określamy pozycję
+        char* pPosition=(char*)screen->pixels ;
+
+        //przesunięcie względem y
+        pPosition+=(screen->pitch*y*2) ;
+
+        //przesunięcie względem x
+        pPosition+=(screen->format->BytesPerPixel*x*2);
+
+        //kopiujemy dane piksela
+        memcpy(&col, pPosition, screen->format->BytesPerPixel);
+
+        //konwertujemy kolor
+        SDL_GetRGB(col, screen->format, &color.r, &color.g, &color.b);
+    }
+    return ( color ) ;
+}
+
+SDL_Color getPixelSurface(int x, int y, SDL_Surface *surface) {
+    SDL_Color color ;
+    Uint32 col = 0 ;
+    if ((x>=0) && (x<szerokosc) && (y>=0) && (y<wysokosc)) {
+        //określamy pozycję
+        char* pPosition=(char*)surface->pixels ;
+
+        //przesunięcie względem y
+        pPosition+=(surface->pitch*y) ;
+
+        //przesunięcie względem x
+        pPosition+=(surface->format->BytesPerPixel*x);
+
+        //kopiujemy dane piksela
+        memcpy(&col, pPosition, surface->format->BytesPerPixel);
+
+        //konwertujemy kolor
+        SDL_GetRGB(col, surface->format, &color.r, &color.g, &color.b);
+    }
+    return ( color ) ;
+}
+
+
+void ladujBMP(char const* nazwa, int x, int y)
+{
+    SDL_Surface* bmp = SDL_LoadBMP(nazwa);
+    if (!bmp)
+    {
+        printf("Unable to load bitmap: %s\n", SDL_GetError());
+    }
+    else
+    {
+        SDL_Color kolor;
+        for (int yy=0; yy<bmp->h; yy++) {
+			for (int xx=0; xx<bmp->w; xx++) {
+				kolor = getPixelSurface(xx, yy, bmp);
+				setPixel(xx, yy, kolor.r, kolor.g, kolor.b);
+			}
+        }
+		SDL_FreeSurface(bmp);
+        SDL_UpdateWindowSurface(window);
+    }
+
+}
+
+
+void czyscEkran(Uint8 R, Uint8 G, Uint8 B)
+{
+    SDL_FillRect(screen, 0, SDL_MapRGB(screen->format, R, G, B));
+    SDL_UpdateWindowSurface(window);
+}
+
+
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+		printf("SDL_Init Error: %s\n", SDL_GetError());
+		return EXIT_FAILURE;
+    }
+
+    window = SDL_CreateWindow(tytul, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, szerokosc*2, wysokosc*2, SDL_WINDOW_SHOWN);
+
+    if (window == NULL) {
+        printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    screen = SDL_GetWindowSurface(window);
+    if (screen == NULL) {
+        fprintf(stderr, "SDL_GetWindowSurface Error: %s\n", SDL_GetError());
+    return false;
+    }
+    SDL_UpdateWindowSurface(window);
+
+
+    bool done = false;
+    SDL_Event event;
+    // główna pętla programu
+    while (SDL_WaitEvent(&event)) {
+        // sprawdzamy czy pojawiło się zdarzenie
+        switch (event.type) {
+            case SDL_QUIT:
+                done = true;
+                break;
+
+            // sprawdzamy czy został wciśnięty klawisz
+            case SDL_KEYDOWN: {
+                // wychodzimy, gdy wciśnięto ESC
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    done = true;
+                if (event.key.keysym.sym == SDLK_1)
+                    Funkcja1();
+                if (event.key.keysym.sym == SDLK_2)
+                    Funkcja2();
+                if (event.key.keysym.sym == SDLK_3)
+                    Funkcja3();
+                if (event.key.keysym.sym == SDLK_4)
+                    Funkcja4();
+                if (event.key.keysym.sym == SDLK_5)
+                    Funkcja5();
+                if (event.key.keysym.sym == SDLK_6)
+                    Funkcja6();
+                if (event.key.keysym.sym == SDLK_7)
+                    Funkcja7();
+                if (event.key.keysym.sym == SDLK_8)
+                    Funkcja8();
+                if (event.key.keysym.sym == SDLK_9)
+                    Funkcja9();
+                if (event.key.keysym.sym == SDLK_a)
+                    ladujBMP("obrazek1.bmp", 0, 0);
+                if (event.key.keysym.sym == SDLK_s)
+                    ladujBMP("obrazek2.bmp", 0, 0);
+                if (event.key.keysym.sym == SDLK_d)
+                    ladujBMP("obrazek3.bmp", 0, 0);
+                if (event.key.keysym.sym == SDLK_f)
+                    ladujBMP("obrazek4.bmp", 0, 0);
+                if (event.key.keysym.sym == SDLK_g)
+                    ladujBMP("obrazek5.bmp", 0, 0);
+                if (event.key.keysym.sym == SDLK_h)
+                    ladujBMP("obrazek6.bmp", 0, 0);
+                if (event.key.keysym.sym == SDLK_b)
+                    czyscEkran(0, 0, 0);
+                else
+                    break;
+               }
+        }
+        if (done) break;
+    }
+
+    if (screen) {
+        SDL_FreeSurface(screen);
+    }
+
+    if (window) {
+        SDL_DestroyWindow(window);
+    }
+
+
+    SDL_Quit();
+    return 0;
+}
